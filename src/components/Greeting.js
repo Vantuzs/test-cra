047d22a16b2d675bf7fedb72efc93a5c6b422b80
Vantuz <Shcherbina444@gmail.com>
2025-01-04 12:11:48 +0200

    Greeting state task resolve

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -4,18 +4,32 @@ class Greeting extends React.Component {
     constructor(props){
         super(props)
 
-        this.state = {
-            
+        this.state = { 
+            isGreet: true
         }
     }
 
     // тут могут быть какието оброботчики событий
+    clickHandler(){
+        this.setState({
+            isGreet: !this.state.isGreet
+        });
+        // console.log(this.state.trfl);
+    }
 
     render(){
-        console.log(this.props);
-        return <h1>Hello {this.props.name}</h1>
+        const isGreets = this.state.isGreet? 'Hello': 'Buy';
+        // console.log(this.props);
+        return <h1 onClick={()=>{this.clickHandler()}}>{isGreets} {this.props.name}</h1>
     
     }
 }
 
 export default Greeting;
+
+
+/* 
+
+Доделать компоненту таким образом, что бы по нажатию на h1 текст "Hello" меняется на "Buy"
+
+*/
