ce7b9162d8087333677979c6ce8b61465e37360d
Vantuz <Shcherbina444@gmail.com>
2025-01-14 18:01:41 +0200

    CX function - className func

--- src/components/TodoList/TodoForm/TodoForm.js
+++ src/components/TodoList/TodoForm/TodoForm.js
@@ -1,21 +1,31 @@
 import React from "react";
 import styles from "./TodoFormStyle.module.css"
 
+// "*" - Запрещенный символ, нужно реализовать вилидацию 
+
 class TodoForm extends React.Component{
     constructor(props){
         super(props)
 
         this.state = {
-            inpValue: ''
+            inpValue: '',
+            isInputValid: true, 
         }
     }
 
     changeImp= (targ)=>{
         const {target: {value}} = targ
 
-        this.setState({
-            inpValue: value
-        })
+        if(value.includes('*') === true){
+            this.setState({
+                isInputValid: false
+            })
+        } else {
+            this.setState({
+                inpValue: value,
+                isInputValid: true
+            })
+        }
     }
 
     submitFnc= (event) =>{
@@ -25,14 +35,56 @@ class TodoForm extends React.Component{
     }
 
     render(){
-        const {inpValue} = this.state
+        const {inpValue,isInputValid} = this.state
+
+        const className = cx({
+            [styles.input]: true,
+            [styles['invalid-inpute']]: !isInputValid 
+        })
+
         return(
             <form onSubmit={this.submitFnc} className={styles.container}>
-            <input type="text" onChange={this.changeImp} value={inpValue} />
-            <button type="submit">Add task</button>
+                <input type="text" 
+                onChange={this.changeImp} 
+                value={inpValue}  
+                // className={`${styles.input} ${isInputValid  ? '': styles['invalid-inpute']}`}
+                className={className}
+
+                />
+                <button type="submit">Add task</button>
             </form>
         )
     }
 }
 
 export default TodoForm;
+
+function cx(objectClassNames){
+    // const cort = Object.entries(objectClassNames);
+    // const filtredArray = cort.filter(([className,condition])=>condition);
+    // const mapArray = filtredArray.map(([className,condition]) => className);
+    // return mapArray.join(' ')
+
+    return Object.entries(objectClassNames)
+    .filter(([className,condition])=>condition)
+    .map(([className,condition]) => className)
+    .join(' ')
+}
+
+/*
+
+objectClassNames = {
+    className1: true,
+    className2: true,
+    className2: false,
+}
+
+[[className1, true], [className2, true], [className3, false] ] 
+=>
+[[className1, true], [className2, true]]
+=>
+[className1, className2]
+=>
+'className1 className2'
+
+*/
