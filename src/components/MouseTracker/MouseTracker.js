90741687aaa0cc4efdda6c9d530325599c9bcc9d
Vantuz <Shcherbina444@gmail.com>
2025-01-28 17:26:34 +0200

    setState callback

--- src/components/MouseTracker/MouseTracker.js
+++ src/components/MouseTracker/MouseTracker.js
@@ -9,7 +9,8 @@ import React, { useState, useEffect } from 'react';
 const MouseTracker = (props) => {
     const [coordinates,setCordinates] = useState({
         x: 0,
-        y: 0
+        y: 0,
+        conter: 0
     });
 
     useEffect(()=>{
@@ -25,21 +26,71 @@ const MouseTracker = (props) => {
     const trakerHandler = (event) =>{
         const {clientX,clientY} = event
 
-        setCordinates({
-            x: clientX,
-            y: clientY
+        setCordinates((prevState)=>{
+            return {
+                ...prevState,
+                x: clientX,
+                y: clientY
+            }
         })
     }
 
-
+    const clickHanlder = (event)=>{
+        setCordinates((prevState)=>{
+            return {
+                ...prevState,
+                conter: coordinates.conter+1
+            }
+        })
+    }
 
 
     return (
         <>
             <p>X: {coordinates.x}</p>   
             <p>Y: {coordinates.y}</p>
+            <button onClick={clickHanlder}>Click ME!</button>
+            <p>conter: {coordinates.conter}</p>
         </>
     );
 }
 
 export default MouseTracker;
+
+
+/* 
+
+Класовый setState
+
+previousState = {
+    x: 0,
+    y: 0,
+    counter: 0
+}
+
+this.setState({
+    counter: this.state.counter + 1
+})
+
+=>
+ 
+{...prevState, ...newState } -->> {x: 0, y: 0, counter: 1}
+
+Хуковый setState // Хуковый setState не будет дополнять попередним станом!
+
+previousState = {
+    x: 0,
+    y: 0,
+    counter: 0
+}
+
+setState({
+    counter: counter + 1
+})
+
+=>
+
+{...newState} -->> {couner: 1}
+
+
+*/
