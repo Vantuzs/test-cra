eb9aa5185198a44efca18f54691c79c28cba56ea
Vantuz <Shcherbina444@gmail.com>
2025-01-29 12:10:17 +0200

    useRef

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,15 @@
 
 import React, { Component } from 'react';
-import Tree from './Tree/Tree'
-import { ThemeContext } from '../contexts/themeContext';
-import { UserContext } from '../contexts/userContext';
-import CONSTANTS from '../constants';
-const { THEMES } = CONSTANTS
+import Square from './Square/Square';
+
 
 
 class App extends Component {
-    constructor(props){
-        super(props)
-
-        this.state = {
-            user: {
-                firstName: 'John',
-                lastName: 'Doe',
-                email: 'johndoe@gmail.com',
-                avatar: 'https://cdn.britannica.com/92/212692-050-D53981F5/labradoodle-dog-stick-running-grass.jpg'
-            },
-            theme: THEMES.LIGHT
-        }
-    }
-
-    logOut = () => {
-        this.setState({
-            user: {}
-        });
-    }
-
-    setTheme = theme => {
-        this.setState({
-            theme
-        });
-    }
+   
 
     render() {
         return (
-            <ThemeContext.Provider value={{
-                theme: this.state.theme,
-                setTheme: this.setTheme
-            }}>
-                <UserContext.Provider value={{
-                    user: this.state.user,
-                    logOut: this.logOut
-                }}>
-                    App
-                    <Tree />
-                </UserContext.Provider>
-            </ThemeContext.Provider>
+            <Square/>
         );
     }
 }
---
+++ src/components/Square/Square.js
@@ -0,0 +1,57 @@
+// Мы вводим число - где то на стронице отображается квадрат этого чилса ++++++++
+
+import React,{ useState, useRef } from 'react';
+
+const Square = () => {
+const [number,setNumber] = useState(1);
+const refContainer = useRef(null);
+
+console.log(refContainer);
+
+    const changeHandler = ({target: {value}})=>{
+        setNumber(Number(value))
+
+        const { current } = refContainer
+
+        if(value >=1 && value <=10){
+            current.style.backgroundColor = 'green'
+        } else if(value >=11&& value <=30){
+            current.style.backgroundColor = 'yellow'
+        } else if(value >30){
+            current.style.backgroundColor = 'red'
+            current.style.color = 'white'
+        } else if(value <0){
+            current.style.backgroundColor = 'pink'
+            current.style.color = 'white'
+        } else {
+            
+        }
+    }
+
+    return (
+        <>
+            <h1>NUmber square</h1>  
+            <input 
+                type='number' 
+                placeholder='Enter number o0o' 
+                value={number} 
+                onChange={changeHandler}
+                ref={refContainer}
+            />
+            <p> Square: {number**2}</p>
+        </>
+    );
+}
+
+export default Square;
+
+
+
+/* 
+
+- Если пользователь вводит число до 10 (включно) - инпут должен быть зеленого цвета
+- Если пользователь вводит число от 11 до 30 (вкл) - инпут должен быть желтого цвета
+- Если пользователь вводит число больше 30 - инпут должен быть красного цвета, цвет текста - белый
+- Если пользователь вводит отрицательноге число - инпут должен быть розового цвета, цвет текста - белый
+
+*/
