753751593a68b27da0e2259284f7b01e03b925c5
Vantuz <Shcherbina444@gmail.com>
2025-02-02 19:55:56 +0200

    useReducer welcome 2

--- src/components/RegistrationFrom/RegistrationFrom.js
+++ src/components/RegistrationFrom/RegistrationFrom.js
@@ -7,6 +7,11 @@ function reducer(state,action){
                 ...state,
                 count: state.count+1
             }
+        } case 'CLICK_DECREMENT':{
+            return{
+                ...state,
+                count: state.count-1
+            }
         }
         default:{
             return state;
@@ -27,14 +32,33 @@ const Clicker = () => {
             type: 'CLICK_INCREMENT'
         }); // В диспатч вы передаете обьекс action
     }
+
+    const decrimentHandler = ()=>{
+        dispatch({
+            type: 'CLICK_DECREMENT'
+        })
+    }
     
     return (
         <>
             <h1>{state.count}</h1>
             <button onClick={clickHandler}>+1</button>
+            <button onClick={decrimentHandler}>-1</button>
         </>
     );
 }
+
+
+/* 
+Задача
+
+Добавить декримент личильника к функционалу
+
++ 1. ПРопишите case в reducer
++ 2. Сделайте оброботчик собьтий нажатия(onClick)кнопки декременту
++ 3. Создайте в верстке кнопку и прикрепите к ней оброботчкий событий, который вы сделали в предыдещем пункте
+
+*/
 
 export default Clicker;
 
